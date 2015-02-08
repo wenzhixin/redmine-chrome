262c982ce8734ce52df072a0ad536365d6f68924
zhixin <wenzhixin2010@gmail.com>
2015-02-08 10:11:12 +0800

    Fix conflict with jQuery and prototype

--- js/copy.js
+++ js/copy.js
@@ -1,26 +1,29 @@
-$(function () {
-    ZeroClipboard.config({swfPath: $('[data-swf-path]').data('swf-path')});
+(function ($) {
 
-    $('pre').each(function () {
-        if (!$(this).find('code').length) {
-            var code = $(this).html();
-            $(this).html('');
-            $('<code></code>').html(code).appendTo($(this));
-        }
-    }).append('<div class="plugin-zclip" title="Click to copy me.">Copy</div>');
-    
-    $('.plugin-zclip').each(function () {
-        var $this = $(this),
-            client = new ZeroClipboard($this[0]);
+    $(function () {
+        ZeroClipboard.config({swfPath: $('[data-swf-path]').data('swf-path')});
 
-        client.on( "ready", function() {
-            client.on('copy', function () {
-                ZeroClipboard.clearData();
-                var $code = $this.parent().find('code').clone();
-                $code.find('.line-numbers').remove();
-                ZeroClipboard.setData('text/plain', $code.text());
-                $this.text('Copied!');
+        $('pre').each(function () {
+            if (!$(this).find('code').length) {
+                var code = $(this).html();
+                $(this).html('');
+                $('<code></code>').html(code).appendTo($(this));
+            }
+        }).append('<div class="plugin-zclip" title="Click to copy me.">Copy</div>');
+
+        $('.plugin-zclip').each(function () {
+            var $this = $(this),
+                client = new ZeroClipboard($this[0]);
+
+            client.on( "ready", function() {
+                client.on('copy', function () {
+                    ZeroClipboard.clearData();
+                    var $code = $this.parent().find('code').clone();
+                    $code.find('.line-numbers').remove();
+                    ZeroClipboard.setData('text/plain', $code.text());
+                    $this.text('Copied!');
+                });
             });
         });
     });
-});
+})(jQuery.noConflict());
